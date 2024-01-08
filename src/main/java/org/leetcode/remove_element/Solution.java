bccf14d01e2559456f17ccb645b9842612dcd70b
Victor Portugal <victorvasconcellos5@hotmail.com>
2024-01-08 18:05:55 -0300

    Remove element problem.

--- src/main/java/org/leetcode/remove_element/Solution.java
+++ src/main/java/org/leetcode/remove_element/Solution.java
@@ -1,3 +1,22 @@
 package org.leetcode.remove_element;
 
-public class Solution {}
+import java.util.Arrays;
+
+public class Solution {
+  public int removeElement(int[] nums, int val) {
+    int i = 0;
+
+    for (final int num : nums) {
+      if (num != val) {
+        nums[i++] = num;
+      }
+    }
+
+    return i;
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.removeElement(new int[] {0, 1, 2, 2, 3, 0, 4, 2}, 2));
+  }
+}
