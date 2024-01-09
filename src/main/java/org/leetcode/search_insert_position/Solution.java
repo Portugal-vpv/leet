c2e3004e7e9388b2a39d47b7afeb2ee2599d5c8b
Victor Portugal <victorvasconcellos5@hotmail.com>
2024-01-09 20:13:29 -0300

    Search insert position problem.

--- src/main/java/org/leetcode/search_insert_position/Solution.java
+++ src/main/java/org/leetcode/search_insert_position/Solution.java
@@ -2,6 +2,19 @@ package org.leetcode.search_insert_position;
 
 public class Solution {
     public int searchInsert(int[] nums, int target) {
+    int high = nums.length - 1;
+    int low = 0;
 
+    while (low <= high) {
+      int mid = (high + low) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (target > nums[mid]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return low;
     }
 }
